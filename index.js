664fd3d581b755824aae2a8d8860d0c22a3102a6
Exequiel Pagliari <exepa@hotmail.com>
2023-05-17 06:40:17 -0300

    probando node.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log(`holamundo`)
+var http = require('http');
+var server = http.createServer();
+ 
+function mensaje(petic, resp) {
+	resp.writeHead(200, {'content-type': 'text/plain'});
+	resp.write('Hola Mundo');
+	resp.end();
+}
+server.on('request', mensaje);
+ 
+server.listen(3000, function () {
+  	console.log('La Aplicación está funcionando en el puerto 3000');
+});
